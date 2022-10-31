95237cd28d8062edaf5cca8ab0208f3576a4f47d
Felipe Camargo <lufelip2@student.42sp.org.br>
2022-10-31 17:26:16 -0300

    basic structure

---
+++ philo/philosophers.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lufelip2 <lufelip2@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/31 17:20:23 by lufelip2          #+#    #+#             */
+/*   Updated: 2022/10/31 17:25:28 by lufelip2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+# include <stdlib.h>
+# include <stdio.h>
+# include <pthread.h>
+
+#endif
